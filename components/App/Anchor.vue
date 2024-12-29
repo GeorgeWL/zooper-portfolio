108241cb853bcd18e89d05f8b887b2353a956bee
GeorgeWL <georgelangham1990@gmail.com>
2024-12-29 20:33:01 +0000

    beter anchor UX

--- components/App/Anchor.vue
+++ components/App/Anchor.vue
@@ -1,5 +1,6 @@
 <template>
-    <a target="_blank" class="underline">
+    <a target="_blank"
+        class="transition hover:underline hover:text-primary-500 dark:hover:text-primary-400 text-primary-600 dark:text-primary-400">
         <slot />
     </a>
 </template>
--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -1,6 +1,7 @@
 <template>
-  <footer class="max-w-2xl mx-auto text-gray-400 dark:text-gray-600 text-sm text-center pb-8">
-    This is a fork of <a href="https://github.com/fayazara/zooper" class="text-blue-500 hover:underline">Zooper</a>
+  <footer class="max-w-2xl mx-auto text-gray-400 dark:text-gray-400 text-sm text-center pb-8">
+    This is a fork of <a href="https://github.com/fayazara/zooper"
+      class=" transition hover:text-primary-500 hover:underline dark:hover:text-primary-400 text-primary-600 dark:text-primary-400">Zooper</a>
     portfolio template, customised for my usage
   </footer>
 </template>
