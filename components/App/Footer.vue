41218c532b429826dce8eb97809f546384ecd3da
GeorgeWL <georgelangham1990@gmail.com>
2024-12-29 19:53:47 +0000

    footer

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -1,8 +1,6 @@
 <template>
-  <footer
-    class="max-w-2xl mx-auto text-gray-400 dark:text-gray-600 text-sm text-center pb-8"
-  >
-    <br>
-    <p>© 2023 Fayaz Ahmed. All rights reserved.</p>
+  <footer class="max-w-2xl mx-auto text-gray-400 dark:text-gray-600 text-sm text-center pb-8">
+    This is a fork of <a href="https://github.com/fayazara/zooper" class="text-blue-500 hover:underline">Zooper</a>
+    portfolio template, customised for my usage
   </footer>
 </template>
